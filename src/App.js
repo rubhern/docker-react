f584d862101a9a682613e9a5bafc855ec87e1480
rubhern <rub3n.hernandez7@gmail.com>
2021-09-02 19:11:24 +0200

    bea message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Test with doker compose
+         HOLA BEA!!!!
         </p>
         <a
           className="App-link"
